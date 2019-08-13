d593a1d2f4d60342a762f459e279ac8b72c294ba
oliburt <oliburt@gmail.com>
2019-08-13 12:53:24 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word.downcase
+    end
+
+    def word_characters
+        word.split('').sort
+    end
+
+    def match(word_array)
+        word_array.select{|word| word.split('').sort == word_characters}
+    end
+end
+
+# listen = Anagram.new("Listen")
+# word_array = %w(enlists google inlets banana)
+
+# Pry.start
